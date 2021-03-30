3a5a38cf766c0f99c2de2c1e5f4a657b64d3315c
doob187 <60312740+doob187@users.noreply.github.com>
2021-03-30 23:24:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,50 @@
-#!/usr/bin/with-contenv bash
-# shellcheck shell=bash
-# Copyright (c) 2021, MrDoob
-# All rights reserved.
-#
-# shellcheck disable=SC2086
-# shellcheck disable=SC2002
-# shellcheck disable=SC2006
+#!/bin/sh
 
-if [[ ! -z "$BACKUP" ]];then
-    echo $BACKUP
-    # Set arguments
-    if [[ "$#" -eq "0" ]];then
-        set -- "$@" $BACKUP
-    fi
+usage() {
+  echo "Usage: docker-volume-backup <backup|restore> <archive>"
+  echo ""
+  echo "Example (backup): docker-volume-backup backup archive"
+  echo "  => this will tar and gzip all files in /volume to /backup/archive.tar.gz"
+  echo ""
+  echo "Example (restore): docker-volume-backup restore archive"
+  echo "  => this will extract all files from /backup/archive.tar.gz" to /volume
+  echo ""
+  exit
+}
+
+backup() {
+  mkdir -p `dirname /backup/$ARCHIVE`
+  tar -czf /backup/$ARCHIVE -C /volume ./
+}
+
+restore() {
+  if ! [ -e /backup/$ARCHIVE ]; then
+    echo "Can't find backup file $ARCHIVE"
+    exit 1
+  fi
+
+  rm -rf /volume/* /volume/..?* /volume/.[!.]*
+  tar -C /volume/ -xzf /backup/$ARCHIVE
+}
+
+sleep 1
+
+if [ $# -ne 2 ]; then
+  usage
 fi
 
-CMD="/usr/bin/echo $@"
-echo $CMD
-$CMD
+OPERATION=$1
 
-# If error, print the error
-if [ $? -ne 0 ];then
-    echo "Here is the output"
-fi
+ARCHIVE=${2%%.tar.gz}.tar.gz
+
+case "$OPERATION" in
+"backup" )
+  backup
+  ;;
+"restore" )
+  restore
+  ;;
+* )
+  usage
+  ;;
+esac
