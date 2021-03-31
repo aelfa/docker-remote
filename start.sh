081a10e7fa598669f28520d13b4a33aafe63cfa8
doob187 <60312740+doob187@users.noreply.github.com>
2021-03-31 22:12:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,30 +1,20 @@
 #!/bin/sh
 
 usage() {
-  echo "Usage: docker-volume-backup <backup|restore> <archive>"
+  echo "Usage: <backup|restore> <appname> <remote>"
   echo ""
-  echo "Example (backup): docker-volume-backup backup archive"
-  echo "  => this will tar and gzip all files in /volume to /backup/archive.tar.gz"
+  echo "Example (backup): backup appname remote"
   echo ""
-  echo "Example (restore): docker-volume-backup restore archive"
-  echo "  => this will extract all files from /backup/archive.tar.gz" to /volume
-  echo ""
+  echo "Example (restore): restore appname remote"
   exit
 }
 
 backup() {
-  mkdir -p `dirname /backup/$ARCHIVE`
-  tar -czf /backup/$ARCHIVE -C /volume ./
+  echo "$1 $2 $3"
 }
 
 restore() {
-  if ! [ -e /backup/$ARCHIVE ]; then
-    echo "Can't find backup file $ARCHIVE"
-    exit 1
-  fi
-
-  rm -rf /volume/* /volume/..?* /volume/.[!.]*
-  tar -C /volume/ -xzf /backup/$ARCHIVE
+  echo "$1 $2 $3"
 }
 
 sleep 1
@@ -34,8 +24,8 @@ if [ $# -ne 2 ]; then
 fi
 
 OPERATION=$1
-
-ARCHIVE=${2%%.tar.gz}.tar.gz
+ARCHIVE=$2.tar.gz
+REMOTE=$3
 
 case "$OPERATION" in
 "backup" )
