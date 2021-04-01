3dc49b71a526ee4c36c004d56e2710b9a06df2d5
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-01 11:54:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,49 @@
-#!/bin/sh
+#!/usr/bin/with-contenv sh
+# shellcheck shell=sh
+# Copyright (c) 2021, MrDoob
+# All rights reserved
+# Idee from scorb/docker-volume-backup
+# customizable: yes
+# fork allowing: yes
+# modification allowed: yes
+######
 
 usage() {
-  echo "Usage: <backup|restore> <appname> <remote>"
+  echo "Usage: <backup|restore|check> <appname> <remote>"
   echo ""
-  echo "Example (backup): backup appname remote"
+  echo "Example (backup): <backup> <appname> <remote>"
   echo ""
-  echo "Example (restore): restore appname remote"
+  echo "Example (restore): <restore> <appname> <remote>"
+  echo ""
+  echo "Example (check): <check> <appname> <remote>"
   exit
 }
 
+## backup specific app
 backup() {
-  echo "testbackup = ${1} ${2} ${3}"
+OPERATION=${OPERATION}
+ARCHIVE=${ARCHIVE}
+REMOTE=${REMOTE}
+  echo "testbackup = ${OPERATION} ${ARCHIVE} ${REMOTE}"
 }
 
+## restore specific app
 restore() {
-  echo "testrestore = ${1} ${2} ${3}"
+OPERATION=${OPERATION}
+ARCHIVE=${ARCHIVE}
+REMOTE=${REMOTE}
+  echo "testrestore = ${OPERATION} ${ARCHIVE} ${REMOTE}"
 }
 
+## check specific app of existing
+check() {
+OPERATION=${OPERATION}
+ARCHIVE=${ARCHIVE}
+REMOTE=${REMOTE}
+  echo "test check = ${OPERATION} ${ARCHIVE} ${REMOTE}"
+}
+
+}
 # CHECK ARE 3 ARGUMENTES #
 if [[ $# -ne 3 ]];then usage;fi
 
@@ -28,6 +55,7 @@ REMOTE=$3
 # RUNNER #
 case "$OPERATION" in
  "backup" ) backup ;;
+ "check" ) check ;;
  "restore" ) restore ;;
  * ) usage ;;
 esac
