04f881b93dcf114712948e4c078ddba48fe7e6aa
doob187 <60312740+doob187@users.noreply.github.com>
2021-04-01 13:32:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,44 +21,70 @@ usage() {
 
 ## backup specific app
 backup() {
+## parser
 OPERATION=${OPERATION}
 ARCHIVE=${ARCHIVE}
 ARCHIVETAR=${ARCHIVE}.tar.gz
 REMOTE=${REMOTE}
-   echo "test backup command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
-        if [[ ! -d /${OPERATION}/${ARCHIVE} ]];then mkdir -p /${OPERATION}/${ARCHIVE};fi
+
+## start
+   echo "test ${OPERATION} command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
+        if [ ! -d /${OPERATION}/${ARCHIVE} ];then mkdir -p /${OPERATION}/${ARCHIVE};fi
    echo "folder /${OPERATION}/${ARCHIVE} created"
+        if [ ! -x "$(command -v rsync)" ] && [ ! -x "$(command -v rclone)" ];then
+        apk --quiet --no-cache --no-progress update && \
+        apk --quiet --no-cache --no-progress upgrade
+        inst="rsync rclone bc"
+        for i in ${inst};do
+            apk --quiet --no-cache --no-progress add $i
+            echo "depends install of $i"
+        done
+        fi
 }
 
 ## restore specific app
 restore() {
+## parser
 OPERATION=${OPERATION}
 ARCHIVE=${ARCHIVE}
 ARCHIVETAR=${ARCHIVE}.tar.gz
 REMOTE=${REMOTE}
-   echo "test restore command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
-         if [[ ! -d /${OPERATION}/${ARCHIVE} ]];then mkdir -p /${OPERATION}/${ARCHIVE};fi
+
+## start
+   echo "test ${OPERATION} command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
+        if [ ! -d /${OPERATION}/${ARCHIVE} ];then mkdir -p /${OPERATION}/${ARCHIVE};fi
    echo "folder /${OPERATION}/${ARCHIVE} created"
+        if [ ! -x "$(command -v rsync)" ] && [ ! -x "$(command -v rclone)" ];then
+        apk --quiet --no-cache --no-progress update && \
+        apk --quiet --no-cache --no-progress upgrade
+        inst="rsync rclone bc"
+        for i in ${inst};do
+            apk --quiet --no-cache --no-progress add $i
+            echo "depends install of $i"
+        done
+        fi
 }
 
 ## check specific app of existing
 check() {
+## parser
 OPERATION=${OPERATION}
 ARCHIVE=${ARCHIVE}
 ARCHIVETAR=${ARCHIVE}.tar.gz
 REMOTE=${REMOTE}
-   echo "test check command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
-        if [[ ! -d /${OPERATION}/${ARCHIVE} ]];then mkdir -p /${OPERATION}/${ARCHIVE};fi
+
+## start
+   echo "test ${OPERATION} command = ${OPERATION} ${ARCHIVE} ${REMOTE}"
+        if [ ! -d /${OPERATION}/${ARCHIVE} ];then mkdir -p /${OPERATION}/${ARCHIVE};fi
    echo "folder /${OPERATION}/${ARCHIVE} created"
 }
 
 # CHECK ARE 3 ARGUMENTES #
-if [[ $# -ne 3 ]];then usage;fi
+if [ $# -ne 3 ];then usage;fi
 
 # ARGUMENTES #
 OPERATION=$1
 ARCHIVE=$2
-ARCHIVETAR=$2.tar.gz
 REMOTE=$3
 
 # RUNNER #
